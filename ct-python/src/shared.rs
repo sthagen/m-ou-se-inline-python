e273ad9bdf7474e8cb8c300093adc1a5c58d9c52
Mara Bos <m-ou.se@m-ou.se>
2025-05-04 13:20:29 +0200

    Remove ct-python->inline-python-macros dependency.

---
+++ ct-python/src/shared.rs
@@ -0,0 +1,1 @@
+../../macros/src/shared.rs
--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,45 +1,31 @@
-//! Helper crate for `inline-python` and `ct-python`.
+//! Helper crate for `inline-python`.
 
 #![feature(proc_macro_span)]
 
 extern crate proc_macro;
 
-use self::embed_python::EmbedPython;
-use proc_macro::{Delimiter, Group, Ident, Literal, Punct, Spacing, Span, TokenStream, TokenTree};
-use pyo3::{Py, PyObject, Python, ffi};
-use std::{ffi::CString, fmt::Display};
+use proc_macro::{Delimiter, Group, Literal, Span, TokenStream, TokenTree};
+use pyo3::{Py, Python};
+use std::{
+	collections::BTreeMap,
+	ffi::{CStr, CString},
+};
 
-mod embed_python;
-mod error;
-mod run;
+mod shared;
+use shared::*;
+
+#[doc(hidden)]
+#[proc_macro]
+pub fn python(input: TokenStream) -> TokenStream {
+	python_impl(input).unwrap_or_else(|e| e)
+}
 
 #[rustfmt::skip]
 fn python_impl(input: TokenStream) -> Result<TokenStream, TokenStream> {
-	let tokens = input.clone();
-
-	let filename = Span::call_site().file();
-
-	let mut x = EmbedPython::new();
-
-	x.add(input)?;
-
-	let EmbedPython { python, variables, .. } = x;
-
-	let python = CString::new(python).unwrap();
-	let filename = CString::new(filename).unwrap();
-
-	let bytecode = unsafe {
-		let result: Result<Literal, TokenStream> = Python::with_gil(|py| {
-			let code = PyObject::from_owned_ptr_or_err(py,
-				ffi::Py_CompileString(python.as_ptr(), filename.as_ptr(), ffi::Py_file_input)
-			).map_err(|err| error::compile_error_msg(py, err, tokens))?;
-			Ok(Literal::byte_string(Py::from_owned_ptr_or_err(py,
-				ffi::PyMarshal_WriteObjectToString(code.as_ptr(), pyo3::marshal::VERSION)
-			).map_err(|_| compile_error(None, "failed to generate bytecode"))?.as_bytes(py)))
-		});
-		result?
-	};
-
+	let mut variables = BTreeMap::new();
+	let python = CString::new(python_from_macro(input.clone(), Some(&mut variables))?).unwrap();
+	let filename = CString::new(Span::call_site().file()).unwrap();
+	let bytecode = compile_to_bytecode(&python, &filename, input)?;
 	Ok(TokenStream::from_iter([
 		punct(':'), punct(':'), ident("inline_python"),
 		punct(':'), punct(':'), ident("FromInlinePython"),
@@ -65,82 +51,17 @@ fn python_impl(input: TokenStream) -> Result<TokenStream, TokenStream> {
 	]))
 }
 
-/// Create a compile_error!{} using two spans that mark the start and end of the error.
-#[rustfmt::skip]
-fn compile_error(spans: Option<(Span, Span)>, error: &(impl Display + ?Sized)) -> TokenStream {
-	let mut tokens = [
-		punct(':'), punct(':'), ident("core"),
-		punct(':'), punct(':'), ident("compile_error"),
-		punct('!'), braces([string(&format!("python: {error}"))]),
-	];
-	if let Some((span1, span2)) = spans {
-		for (i, t) in tokens.iter_mut().enumerate() {
-			t.set_span(if i < 6 { span1 } else { span2 });
-		}
-	}
-	TokenStream::from_iter(tokens)
-}
-
-fn ct_python_impl(input: TokenStream) -> Result<TokenStream, TokenStream> {
-	let tokens = input.clone();
-
-	let filename = Span::call_site().file();
-
-	let mut x = EmbedPython::new();
-
-	x.compile_time = true;
-
-	x.add(input)?;
-
-	let EmbedPython { python, .. } = x;
-
-	let python = CString::new(python).unwrap();
-	let filename = CString::new(filename).unwrap();
-
-	Python::with_gil(|py| {
-		let code = unsafe {
-			PyObject::from_owned_ptr_or_err(py, ffi::Py_CompileString(python.as_ptr(), filename.as_ptr(), ffi::Py_file_input))
-				.map_err(|err| error::compile_error_msg(py, err, tokens.clone()))?
-		};
-
-		run::run_ct_python(py, code, tokens)
-	})
-}
-
-#[doc(hidden)]
-#[proc_macro]
-pub fn python(input: TokenStream) -> TokenStream {
-	match python_impl(input) {
-		Ok(tokens) => tokens,
-		Err(tokens) => tokens,
-	}
-}
-
-#[doc(hidden)]
-#[proc_macro]
-pub fn ct_python(input: TokenStream) -> TokenStream {
-	match ct_python_impl(input) {
-		Ok(tokens) => tokens,
-		Err(tokens) => tokens,
-	}
-}
-
-fn punct(p: char) -> TokenTree {
-	TokenTree::Punct(Punct::new(p, Spacing::Joint))
-}
-
-fn ident(s: &str) -> TokenTree {
-	TokenTree::Ident(Ident::new(s, Span::call_site()))
-}
-
 fn parens(t: impl IntoIterator<Item = TokenTree>) -> TokenTree {
 	TokenTree::Group(Group::new(Delimiter::Parenthesis, TokenStream::from_iter(t)))
 }
 
-fn braces(t: impl IntoIterator<Item = TokenTree>) -> TokenTree {
-	TokenTree::Group(Group::new(Delimiter::Brace, TokenStream::from_iter(t)))
-}
-
-fn string(s: &str) -> TokenTree {
-	TokenTree::Literal(Literal::string(s))
+fn compile_to_bytecode(python: &CStr, filename: &CStr, tokens: TokenStream) -> Result<Literal, TokenStream> {
+	Python::with_gil(|py| {
+		let compiled = compile_python(py, python, filename, tokens)?;
+		let bytes = unsafe {
+			let ptr = pyo3::ffi::PyMarshal_WriteObjectToString(compiled.as_ptr(), pyo3::marshal::VERSION);
+			Py::from_owned_ptr(py, ptr)
+		};
+		Ok(Literal::byte_string(bytes.as_bytes(py)))
+	})
 }
